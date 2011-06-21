3ef1c330f96a5f9ee58645e68a58c30c5720a41e
Enrico Thierbach <eno@open-lab.org>
2011-06-21 12:25:13 +0200

    Creates and passes around less data
    ...by making the plastic_helper a module.

--- lib/plastic_helper.rb
+++ lib/plastic_helper.rb
@@ -6,54 +6,40 @@ module Rack
   # This class basically contains all of the Rack code that you'd have to write every
   # time you wanted to write a new middleware that modifies the HTML response.
   #
-  class PlasticHelper #:nodoc:
+  module PlasticHelper #:nodoc:
 
-    def request
-      @request
-    end
-
-    def options
-      @options
-    end
-
-    private
-
-    def handle_request(env, app, options, plastic)
-      @app = app
-      @options = options
-      @request = Rack::Request.new(env)
+    # Rack::Plastic provides a call method so that your middleware doesn't
+    # have to.
+    #
+    def call(env)
       status, @headers, @body = @app.call(env)
       if html?
-        if plastic.respond_to? :change_nokogiri_doc
-            doc = Nokogiri::HTML(body_to_string)
-          doc = plastic.send(:change_nokogiri_doc, doc)
+        @request = Rack::Request.new(env)
+
+        new_body_string = @body.join("")
+
+        if respond_to? :change_nokogiri_doc
+            doc = Nokogiri::HTML(new_body_string)
+          doc = change_nokogiri_doc(doc)
           if !doc.is_a?(Nokogiri::XML::Document)
             raise "You must return a Nokogiri::XML::Document object from change_nokogiri_doc."
           end
           new_body_string = doc.to_html
-        else
-          new_body_string = body_to_string
         end
-        if plastic.respond_to? :change_html_string
-          new_body_string = plastic.send(:change_html_string, new_body_string)
-          if !new_body_string.is_a?(String)
-            raise "You must return a String from change_html_string."
-          end
+
+        if respond_to? :change_html_string
+          new_body_string = change_html_string(new_body_string)
+          raise "You must return a String from change_html_string." unless new_body_string.respond_to?(:to_s)
         end
-        update_response_body(new_body_string)
-        update_content_length
+        update_response_body(new_body_string.to_s)
       end
       [status, @headers, @body]
     end
 
+    private
+
     def html?
       @headers["Content-Type"] && @headers["Content-Type"].include?("text/html")
-    end
-
-    def body_to_string
-      s = ""
-      @body.each { |x| s << x }
-      s
     end
 
     def update_response_body(new_body_string)
@@ -64,16 +50,9 @@ module Rack
       else
         @body = [new_body_string]
       end
-    end
 
-    def update_content_length
-      length = 0
-      @body.each do |s|   # we can't use inject because @body may not respond to it
-        length += Rack::Utils.bytesize(s)   # we use Rack::Utils.bytesize to avoid
-                                            # incompatibilities between Ruby 1.8 and 1.9
-      end
-      @headers['Content-Length'] = length.to_s
+      # we use Rack::Utils.bytesize to avoid incompatibilities between Ruby 1.8 and 1.9
+      @headers['Content-Length'] = Rack::Utils.bytesize(new_body_string).to_s
     end
-
   end
 end
